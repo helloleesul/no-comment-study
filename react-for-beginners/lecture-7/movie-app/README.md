90f114aed52fa9967f9712822b7876cb1fb6ebf9
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-06-04 03:48:37 +0900

    react-for-beginners 기록

--- react-for-beginners/lecture-7/movie-app/README.md
+++ react-for-beginners/lecture-7/movie-app/README.md
@@ -1,4 +1,14 @@
-# Loading
+# 업데이트 한 것
+
+## Loading
 
 component/Home 에서 API를 불러오는 코드를 상위 요소인 App 으로 옮겨서
-영화를 클릭하고 다시 home 경로로 돌아와도 새로 loading 되지 않게 수정
+영화를 클릭하고 다시 home 경로로 돌아와도 새로 loading 되지 않게 수정함
+
+# 해야할 것
+
+- [ ] 현재 디렉토리 새 저장소로 클론하기 [참고1](https://infiduk.github.io/2022/02/09/git.html) [참고2](https://www.lesstif.com/gitbook/git-clone-20776761.html)
+- [ ] Detail.js 마주친 에러 기록하기 [참고](https://velog.io/@rgfdds98/debuging-React-Hook-useEffect-has-a-missing-dependency-fetchMovieData.-Either-include-it-or-remove-the-dependency-array)
+- [ ] 영화상세정보 컴포넌트에서 로딩될 때 스켈레톤 ui 구현하기
+- [ ] 클론한 저장소에서 배포하기
+- [ ] 스타일 입히기
--- react-for-beginners/lecture-7/movie-app/src/routes/Detail.js
+++ react-for-beginners/lecture-7/movie-app/src/routes/Detail.js
@@ -4,15 +4,15 @@ import { useEffect, useState } from "react";
 function Detail() {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    setMovie(json.data.movie);
-  };
   useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+    };
     getMovie();
-  }, []);
+  }, [id]);
   return <h1>{movie.title}</h1>;
 }
 
